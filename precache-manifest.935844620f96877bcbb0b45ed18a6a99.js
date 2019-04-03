7411c7c5ddc452b2e4c8a7b2114809b2fcbe0adb
AceelAlArifi <aceelalarifi@gmail.com>
2019-04-03 16:17:50 +0300

    Updates

---
+++ precache-manifest.935844620f96877bcbb0b45ed18a6a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a2f889d8b7c0e0a2cb",
+    "url": "/Garage-Record/static/js/runtime~main.fce0d297.js"
+  },
+  {
+    "revision": "347cc6b0604b142b4ced",
+    "url": "/Garage-Record/static/js/main.0ad0ddd1.chunk.js"
+  },
+  {
+    "revision": "960f85572ffe81274e14",
+    "url": "/Garage-Record/static/js/2.70d8af80.chunk.js"
+  },
+  {
+    "revision": "347cc6b0604b142b4ced",
+    "url": "/Garage-Record/static/css/main.de98274b.chunk.css"
+  },
+  {
+    "revision": "960f85572ffe81274e14",
+    "url": "/Garage-Record/static/css/2.696684ee.chunk.css"
+  },
+  {
+    "revision": "fa0058f28521ba19c331a7822a750d96",
+    "url": "/Garage-Record/index.html"
+  }
+];
